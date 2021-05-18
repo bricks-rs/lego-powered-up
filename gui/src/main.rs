4f01697db6652899c470fb6f19b3886649c275ec
David Young <david@thedavidyoung.co.uk>
2021-05-18 22:36:14 +0100

    Add basic framework code to spawn UI

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,3 +1,28 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(startup_system.system())
+        .run();
+}
+
+fn startup_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // UI camera
+    commands.spawn_bundle(UiCameraBundle::default());
+
+    // root node
+    /*commands.spawn_bundle(NodeBundle {
+        style: Style {
+            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+            justify_content: JustifyContent::SpaceBetween,
+            ..Default::default()
+        },
+        material: materials.add(Color::NONE.into()),
+        ..Default::default()
+    });*/
 }
