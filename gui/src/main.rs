3027cec59866dccf51ecb98a4c7d36f4b571c4df
David Young <david@thedavidyoung.co.uk>
2021-05-22 16:02:35 +0100

    Switch ui components over to bevy_egui

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,185 +1,63 @@
 use bevy::prelude::*;
+use bevy_egui::{egui, EguiContext, EguiPlugin, EguiSettings};
 
 fn main() {
-    //env_logger::init();
     App::build()
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .insert_resource(Msaa { samples: 4 })
+        .init_resource::<UiState>()
         .add_plugins(DefaultPlugins)
         .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin::default())
         .add_plugin(bevy::diagnostic::LogDiagnosticsPlugin::default())
-        .init_resource::<ButtonMaterials>()
+        .add_plugin(EguiPlugin)
         .add_startup_system(startup_system.system())
-        .add_system(button_system.system())
+        .add_system(update_ui_scale_factor.system())
+        .add_system(ui.system())
         .run();
 }
 
-struct ButtonMaterials {
-    normal: Handle<ColorMaterial>,
-    hovered: Handle<ColorMaterial>,
-    pressed: Handle<ColorMaterial>,
-}
-
-impl FromWorld for ButtonMaterials {
-    fn from_world(world: &mut World) -> Self {
-        let mut materials =
-            world.get_resource_mut::<Assets<ColorMaterial>>().unwrap();
-        ButtonMaterials {
-            normal: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
-            hovered: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
-            pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
-        }
-    }
-}
-
-fn button_system(
-    button_materials: Res<ButtonMaterials>,
-    mut interaction_query: Query<
-        (&Interaction, &mut Handle<ColorMaterial>, &Children),
-        (Changed<Interaction>, With<Button>),
-    >,
-    mut text_query: Query<&mut Text>,
-) {
-    for (interaction, mut material, children) in interaction_query.iter_mut() {
-        let mut text = text_query.get_mut(children[0]).unwrap();
-        match *interaction {
-            Interaction::Clicked => {
-                text.sections[0].value = "Press".to_string();
-                *material = button_materials.pressed.clone();
-            }
-            Interaction::Hovered => {
-                text.sections[0].value = "Hover".to_string();
-                *material = button_materials.hovered.clone();
-            }
-            Interaction::None => {
-                text.sections[0].value = "Button".to_string();
-                *material = button_materials.normal.clone();
-            }
-        }
-    }
+#[derive(Default)]
+struct UiState {
+    label: String,
 }
 
 fn startup_system(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    button_materials: Res<ButtonMaterials>,
 ) {
-    // UI camera
-    commands.spawn_bundle(UiCameraBundle::default());
-
-    // root node
-    commands
-        .spawn_bundle(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                justify_content: JustifyContent::SpaceBetween,
-                ..Default::default()
-            },
-            material: materials.add(Color::NONE.into()),
-            ..Default::default()
-        })
-        .with_children(|p| make_menu_bar(p, &asset_server, button_materials))
-        .with_children(|parent| {
-            // left vertical fill (border)
-            parent
-                .spawn_bundle(NodeBundle {
-                    style: Style {
-                        size: Size::new(Val::Px(200.0), Val::Percent(90.0)),
-                        border: Rect::all(Val::Px(2.0)),
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                })
-                .with_children(|parent| {
-                    // left vertical fill (content)
-                    parent
-                        .spawn_bundle(NodeBundle {
-                            style: Style {
-                                size: Size::new(
-                                    Val::Percent(100.0),
-                                    Val::Percent(100.0),
-                                ),
-                                align_items: AlignItems::FlexEnd,
-                                ..Default::default()
-                            },
-                            material: materials
-                                .add(Color::rgb(0.15, 0.15, 0.15).into()),
-                            ..Default::default()
-                        })
-                        .with_children(|parent| {
-                            // text
-                            parent.spawn_bundle(TextBundle {
-                                style: Style {
-                                    margin: Rect::all(Val::Px(5.0)),
-                                    ..Default::default()
-                                },
-                                text: Text::with_section(
-                                    "Lost the game!",
-                                    TextStyle {
-                                        font: asset_server
-                                            .load("fonts/FiraSans-Bold.ttf"),
-                                        font_size: 30.0,
-                                        color: Color::WHITE,
-                                    },
-                                    Default::default(),
-                                ),
-                                ..Default::default()
-                            });
-                        });
-                });
-        });
 }
 
-fn make_menu_bar(
-    parent: &mut ChildBuilder,
-    asset_server: &Res<AssetServer>,
-    button_materials: Res<ButtonMaterials>,
+fn update_ui_scale_factor(
+    mut egui_settings: ResMut<EguiSettings>,
+    windows: Res<Windows>,
 ) {
-    const BUTTON_HEIGHT: Val = Val::Percent(100.0);
-    const BUTTON_FONT_SIZE: f32 = 20.0;
+    if let Some(window) = windows.get_primary() {
+        egui_settings.scale_factor = 1.0 / window.scale_factor();
+    }
+}
 
-    // Background
-    parent
-        .spawn_bundle(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Percent(100.0), Val::Px(30.0)),
-                border: Rect::all(Val::Px(2.0)),
-                position_type: PositionType::Absolute,
-                position: Rect {
-                    left: Val::Px(0.0),
-                    top: Val::Px(0.0),
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .with_children(|parent| {
-            parent
-                .spawn_bundle(ButtonBundle {
-                    style: Style {
-                        size: Size::new(Val::Auto, BUTTON_HEIGHT),
-                        margin: Rect::all(Val::Auto),
-                        justify_content: JustifyContent::Center,
-                        align_items: AlignItems::Center,
-                        ..Default::default()
-                    },
-                    material: button_materials.normal.clone(),
-                    ..Default::default()
-                })
-                .with_children(|parent| {
-                    parent.spawn_bundle(TextBundle {
-                        text: Text::with_section(
-                            "GAAME",
-                            TextStyle {
-                                font: asset_server
-                                    .load("fonts/FiraSans-Bold.ttf"),
-                                font_size: BUTTON_FONT_SIZE,
-                                color: Color::GREEN,
-                            },
-                            Default::default(),
-                        ),
-                        ..Default::default()
-                    });
-                });
+fn ui(
+    mut egui_ctx: ResMut<EguiContext>,
+    mut ui_state: ResMut<UiState>,
+    assets: Res<AssetServer>,
+) {
+    // Menu bar
+    egui::TopPanel::top("Top panel").show(egui_ctx.ctx(), |ui| {
+        egui::menu::bar(ui, |ui| {
+            egui::menu::menu(ui, "File", |ui| {
+                if ui.button("Quit").clicked() {
+                    std::process::exit(0);
+                }
+            });
         });
+    });
+
+    egui::SidePanel::left("side_panel", 200.0).show(egui_ctx.ctx(), |ui| {
+        ui.heading("Side panel");
+
+        ui.horizontal(|ui| {
+            ui.label("GAME");
+        });
+    });
 }
