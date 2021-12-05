7c2ce5145f60504cf5ebbb5de04c36d72d2a5552
David Young <david@thedavidyoung.co.uk>
2021-12-05 22:07:03 +0000

    Add MPL-2.0 headers

--- lego-powered-up/src/lib.rs
+++ lego-powered-up/src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! # lego-powered-up
 //! This is a crate for remotely controlling Lego PoweredUp hubs and devices
 //! over Bluetooth. It provides a simple synchronous interface, with the
--- pu-util/src/motor_test.rs
+++ pu-util/src/motor_test.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use crate::argparse::MotorTestArgs;
 use anyhow::Result;
 use lego_powered_up::notifications::Power;
