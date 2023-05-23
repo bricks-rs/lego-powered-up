2284dda27a8f523fb9718e2aed07850a014d7a80
joakim000 <joakim.odermalm@gmail.com>
2023-05-23 20:51:50 +0200

    Remote control example

--- examples/gamepad/src/main.rs
+++ examples/gamepad/src/main.rs
@@ -1,169 +1,110 @@
 // Any copyright is dedicated to the Public Domain.
 // https://creativecommons.org/publicdomain/zero/1.0/
+#![allow(unused)]
+use core::panic;
+use std::println;
 
-use gilrs::{Button, Event, EventType::AxisChanged, Gilrs};
-use lego_powered_up::{
-    devices::Device, hubs::Hub, notifications::Power, PoweredUp,
-};
-use std::fmt::{self, Display, Formatter};
-use std::time::{Duration, Instant};
+use lego_powered_up::{notifications::Power, PoweredUp, btleplug::{platform::Manager}, devices};
+use lego_powered_up::{HubFilter, notifications::EndState, notifications::NotificationMessage};
+// use lego_powered_up::{DiscoveredHub};
+use lego_powered_up::{btleplug::api::{Central, CentralEvent, ScanFilter, Manager as _, Peripheral as _, PeripheralProperties}};
+use lego_powered_up::{btleplug::api::Peripheral};
+use lego_powered_up::{btleplug::api::{CharPropFlags, ValueNotification}};
+use lego_powered_up::{futures::stream::{StreamExt, FuturesUnordered}};
+use lego_powered_up::{futures::{future, select}};
 
-struct Robot {
-    speed_proportion: f32,
-    steering_proportion: f32,
-    left_speed: i8,
-    right_speed: i8,
-    left_motor: Box<dyn Device>,
-    right_motor: Box<dyn Device>,
-    changed: bool,
-}
-
-impl Display for Robot {
-    fn fmt(&self, fmt: &mut Formatter) -> fmt::Result {
-        write!(fmt, "{:3} {:3}", self.left_speed, self.right_speed)
-    }
-}
-
-impl Robot {
-    pub async fn set_steering(&mut self, steering: f32) {
-        self.steering_proportion = steering.clamp(-1.0, 1.0);
-        self.update();
-    }
-
-    pub async fn set_speed(&mut self, speed: f32) {
-        self.speed_proportion = speed.clamp(-1.0, 1.0);
-        self.update();
-    }
-
-    pub async fn new(
-        hub: &dyn Hub,
-    ) -> Result<Self, Box<dyn std::error::Error>> {
-        let left_motor = hub.port(lego_powered_up::hubs::Port::C).await?;
-        let right_motor = hub.port(lego_powered_up::hubs::Port::D).await?;
-        Ok(Self {
-            speed_proportion: 0.0,
-            steering_proportion: 0.0,
-            left_speed: 0,
-            right_speed: 0,
-            left_motor,
-            right_motor,
-            changed: false,
-        })
-    }
-
-    // speed proportion is scaled to range [-80, 80]
-    // steering proportion is scaled to range [-20, 20] and then
-    // added/subtracted
-    pub async fn send(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        self.left_motor
-            .start_speed(self.left_speed, Power::from_i8(self.left_speed)?)
-            .await?;
-        self.right_motor
-            .start_speed(self.right_speed, Power::from_i8(self.right_speed)?)
-            .await?;
-        Ok(())
-    }
-
-    pub fn changed(&mut self) -> bool {
-        let c = self.changed;
-        self.changed = false;
-        c
-    }
-
-    fn update(&mut self) {
-        let base_speed = (self.speed_proportion * 80.0).round() as i8;
-        let mut steering_factor =
-            (self.steering_proportion * 20.0).round() as i8;
-
-        // if driving in reverse then invert the steering factor to make
-        // the steering direction more natural
-        if base_speed < 0 {
-            steering_factor *= -1;
-        }
-
-        // if driving speed is close to 0 then boost the steering factor
-        // a bit to make spinning in place more fun
-        if base_speed.abs() < 20 {
-            steering_factor *= 3;
-        }
-
-        let left_speed = base_speed + steering_factor;
-        let right_speed = -(base_speed - steering_factor);
-
-        self.changed = (self.left_speed != left_speed)
-            || (self.right_speed != right_speed);
-        self.left_speed = left_speed;
-        self.right_speed = right_speed;
-    }
-}
+// use btleplug::api::*;
+use std::time::Duration;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
-    let mut gilrs = Gilrs::new().unwrap();
-
-    // Iterate over all connected gamepads
-    for (_id, gamepad) in gilrs.gamepads() {
-        println!("{} is {:?}", gamepad.name(), gamepad.power_info());
-    }
-
-    let mut active_gamepad = None;
-
-    println!("Searching for hubs...");
-    let mut pu = PoweredUp::init().await?;
-    let hub = pu.wait_for_hub().await?;
-
-    println!("Connecting to hub `{}`", hub.name);
-    let hub = pu.create_hub(&hub).await?;
-
-    println!("Change the hub LED to green");
-    let mut hub_led = hub.port(lego_powered_up::hubs::Port::HubLed).await?;
-    hub_led.set_rgb(&[0, 0xff, 0]).await?;
-
-    let mut robot = Robot::new(hub.as_ref()).await?;
-
-    let mut last_update = Instant::now();
-    let min_time_between_updates = Duration::from_millis(100);
-
-    loop {
-        // Examine new events
-        while let Some(Event { id, event, time }) = gilrs.next_event() {
-            #[cfg(debug_assertions)]
-            println!("{:?} New event from {}: {:?}", time, id, event);
-            active_gamepad = Some(id);
-            if let AxisChanged(axis, pos, _) = event {
-                match axis {
-                    gilrs::Axis::LeftStickX => robot.set_steering(pos).await,
-                    gilrs::Axis::LeftStickY => robot.set_speed(pos).await,
-                    gilrs::Axis::LeftZ => (),
-                    gilrs::Axis::RightStickX => (),
-                    gilrs::Axis::RightStickY => (),
-                    gilrs::Axis::RightZ => (),
-                    gilrs::Axis::DPadX => (),
-                    gilrs::Axis::DPadY => (),
-                    gilrs::Axis::Unknown => (),
+async fn main() -> anyhow::Result<()> {
+    // List adapters
+    let adapters = PoweredUp::adapters().await; 
+    match adapters {
+        Result::Ok(list) => {
+            if list.len() > 1 {
+                println!("Found multiple adapters:\n");
+                for a in list.into_iter() {
+                    println!("{}",a.adapter_info().await?);
                 }
+                panic!("Multiple adapters found, don't know which to use.");
+            } else {
+                let a = list.into_iter().next().unwrap();
+                println!("Found adapter: {}.", a.adapter_info().await?);
             }
-        }
-
-        if last_update.elapsed() > min_time_between_updates {
-            last_update = Instant::now();
-            if robot.changed() {
-                println!("{}", robot);
-                robot.send().await?;
+        },    
+        Err(error) => {
+                println!("No adapters found: {}", error);
+                panic!("No adapters found: {}", error);
             }
-        }
-
-        // You can also use cached gamepad state
-        if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
-            if gamepad.is_pressed(Button::South) {
-                println!("Button South is pressed (XBox - A, PS - X)");
-                break;
-            }
-        }
     }
+    let adapter = PoweredUp::adapters().await?.into_iter().next().unwrap();
+    // Init PoweredUp with found adapter
+    let mut pu = PoweredUp::with_adapter(adapter).await?;
 
-    hub.disconnect().await?;
+    let discovered_hubs = pu.wait_for_hubs_filter(HubFilter::Null, 1).await?;
+   
+    let mut dh_iter = discovered_hubs.into_iter();
+    let dh1 = dh_iter.next().unwrap();
+    println!("Connecting to hub `{}`", dh1.name);
+    let hub1 = pu.create_hub(&dh1).await?;
+    // dbg!(hub1.properties());
+    let hub1_name = hub1.name().await?;
+    let mut hub1_stream =
+                        hub1.peripheral().notifications().await?;  
+
+
+    tokio::spawn(async move {
+        while let Some(data) = hub1_stream.next().await {
+            // println!("Received data from {:?} [{:?}]: {:?}", &hub1_name, data.uuid, data.value);
+            println!("{}", &hub1_name);
+            let n = NotificationMessage::parse(&data.value).unwrap();
+            dbg!(&n);
+        }  
+    });
+
+
+
+    let mut remote_a = hub1.port(lego_powered_up::hubs::Port::A).await?;
+    // remote_b.request_port_info(lego_powered_up::notifications::InformationType::PortValue).await?;
+    // remote_b.request_port_info(lego_powered_up::notifications::InformationType::ModeInfo).await?;
+    // remote_b.request_port_info(lego_powered_up::notifications::InformationType::PossibleModeCombinations).await?;
+
+    // remote_b.request_mode_info(0, lego_powered_up::notifications::ModeInformationType::ValueFormat).await?;
+    // remote_b.request_mode_info(1, lego_powered_up::notifications::ModeInformationType::ValueFormat).await?;
+    // remote_b.request_mode_info(2, lego_powered_up::notifications::ModeInformationType::ValueFormat).await?;
+    // remote_b.request_mode_info(3, lego_powered_up::notifications::ModeInformationType::ValueFormat).await?;
+    // remote_b.request_mode_info(4, lego_powered_up::notifications::ModeInformationType::ValueFormat).await?;
+
+    println!("remote buttons mode 0");
+    remote_a.remote_buttons_enable(4, 1).await?;
+    tokio::time::sleep(Duration::from_secs(30)).await;
+    
+    // println!("remote buttons mode 1");
+    // remote_b.remote_buttons_enable(1, 1).await?;
+    // tokio::time::sleep(Duration::from_secs(10)).await;
+
+    // println!("remote buttons mode 2");
+    // remote_b.remote_buttons_enable(2, 1).await?;
+    // tokio::time::sleep(Duration::from_secs(10)).await;
+
+    // println!("remote buttons mode 3");
+    // remote_b.remote_buttons_enable(3, 1).await?;
+    // tokio::time::sleep(Duration::from_secs(10)).await;
+
+    // println!("remote buttons mode 4");
+    // remote_b.remote_buttons_enable(4, 1).await?;
+    // tokio::time::sleep(Duration::from_secs(10)).await;
+
+
+
+    // loop {}
+
+    println!("Disconnect from hub `{}`", hub1.name().await?);
+    hub1.disconnect().await?;
+    
+    println!("Done!");
 
     Ok(())
 }
+
