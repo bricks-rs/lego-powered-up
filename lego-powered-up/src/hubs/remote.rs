7a67b419e8a686406225e0024e7f92078eae0014
joakim000 <joakim.odermalm@gmail.com>
2023-05-25 13:03:55 +0200

    add status struct

--- lego-powered-up/src/hubs/remote.rs
+++ lego-powered-up/src/hubs/remote.rs
@@ -169,3 +169,34 @@ impl RemoteControl {
     // None
     // }
 }
+#[derive(Debug, Copy, Clone)]
+pub struct RemoteStatus {
+    // Buttons
+    pub a_plus: bool,
+    pub a_red: bool,
+    pub a_minus: bool,
+    pub green: bool,
+    pub b_plus: bool,
+    pub b_red: bool,
+    pub b_minus: bool,
+    // Operaional status
+    pub battery: u8,   // 0 - 100 %
+    pub rssi: i8,      // -127 - 0  
+}
+
+impl RemoteStatus {
+    pub fn new() -> Self {
+        Self {
+            a_plus: false,
+            a_red: false,
+            a_minus: false,
+            green: false,
+            b_plus: false,
+            b_red: false,
+            b_minus: false,
+            battery: 100,
+            rssi: 0
+        }
+
+    }
+}
