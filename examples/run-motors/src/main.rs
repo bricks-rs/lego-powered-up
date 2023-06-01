9605f390c0e5dccdc5264bab74b3bb4c4101c24a
joakim000 <joakim.odermalm@gmail.com>
2023-06-01 13:21:12 +0200

    Update run-motors example

--- examples/run-motors/src/main.rs
+++ examples/run-motors/src/main.rs
@@ -1,8 +1,12 @@
 // Any copyright is dedicated to the Public Domain.
 // https://creativecommons.org/publicdomain/zero/1.0/
 
-use lego_powered_up::{notifications::Power, PoweredUp};
-use std::time::Duration;
+use lego_powered_up::{PoweredUp, ConnectedHub, IoTypeId, IoDevice,
+                    consts,
+                    devices::light::{self, HubLed},
+                    devices::motor::{EncoderMotor, Power} 
+                };
+use core::time::Duration;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -11,27 +15,41 @@ async fn main() -> anyhow::Result<()> {
     let hub = pu.wait_for_hub().await?;
 
     println!("Connecting to hub `{}`", hub.name);
-    let hub = pu.create_hub(&hub).await?;
+    let hub = ConnectedHub::setup_hub
+                                        (pu.create_hub(&hub).await.expect("Error creating hub"))
+                                        .await.expect("Error setting up hub");
+    tokio::time::sleep(Duration::from_secs(1)).await;  //Wait for attached devices to be collected
 
+    // Devices to be used
+    let hub_led: IoDevice;
+    let motor_c: IoDevice;
+    let motor_d: IoDevice;
+    {
+        let lock = hub.mutex.lock().await;
+        hub_led = lock.io_from_kind(IoTypeId::HubLed).await?;
+        motor_c = lock.io_from_port(consts::named_port::A).await?; 
+        motor_d = lock.io_from_port(consts::named_port::B).await?; 
+    }    
+    
     println!("Change the hub LED to green");
-    let mut hub_led = hub.port(lego_powered_up::hubs::Port::HubLed).await?;
-    hub_led.set_rgb(&[0, 0xff, 0]).await?;
-
+    hub_led.set_hubled_mode(light::HubLedMode::Colour).await?;
+    hub_led.set_hubled_color(consts::Color::Green).await?;
+   
     println!("Run motors");
-    let mut motor_c = hub.port(lego_powered_up::hubs::Port::C).await?;
-    let mut motor_d = hub.port(lego_powered_up::hubs::Port::D).await?;
     motor_c.start_speed(50, Power::Cw(50)).await?;
     motor_d.start_speed(50, Power::Cw(50)).await?;
 
     tokio::time::sleep(Duration::from_secs(3)).await;
 
     println!("Stop motors");
-    motor_c.start_speed(0, Power::Float).await?;
-    motor_d.start_speed(0, Power::Brake).await?;
+    motor_c.start_power(Power::Float).await?;
+    motor_d.start_power(Power::Brake).await?;
 
-    println!("Disconnect from hub `{}`", hub.name().await?);
-    hub.disconnect().await?;
-
+    println!("Disconnect from hub `{}`", hub.name);
+    {
+        let lock = hub.mutex.lock().await;
+        lock.disconnect().await?;
+    }
     println!("Done!");
 
     Ok(())
